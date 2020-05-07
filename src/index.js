4a98c1b2f77981357c542aa1c52e61f28b187322
chenyuting <chenyuting@tuniu.com>
2020-05-07 16:15:30 +0800

    1.0.1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import './style.css';
 import './style.scss';  //导入css
 import React from 'react';
 import {render} from 'react-dom';
-
 import Mycomponent from './component';
+
 render(<Mycomponent />, document.getElementById('root'));
